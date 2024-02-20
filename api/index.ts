7cd44baa879b98cee1a5c204d4167ff1e35607f3
Zhuravlev Petr <zeterfrost@gmail.com>
2024-02-20 15:57:38 +0600

    fixed bug with multer

--- api/index.ts
+++ api/index.ts
@@ -12,6 +12,7 @@ const app = express();
 const port = 8000;
 
 app.use(json());
+app.use(express.static('public'));
 app.use(cors());
 
 app.use('/artists', artistsRouter);
@@ -21,15 +22,15 @@ app.use('/users', usersRouter);
 app.use('/track_history', trackHistoryRouter);
 
 const run = async () => {
-  await mongoose.connect('mongodb://localhost/spotify');
+	await mongoose.connect('mongodb://localhost/spotify');
 
-  app.listen(port, () => {
-    console.log(`Server started on ${port} port!`);
-  });
+	app.listen(port, () => {
+		console.log(`Server started on ${port} port!`);
+	});
 
-  process.on('exit', () => {
-    mongoose.disconnect();
-  });
+	process.on('exit', () => {
+		mongoose.disconnect();
+	});
 };
 
 void run();
