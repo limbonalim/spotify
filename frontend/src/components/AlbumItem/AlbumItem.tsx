b412321452b3e9f46af6d1a03f57a428f33f7ba1
Zhuravlev Petr <zeterfrost@gmail.com>
2024-02-22 11:03:15 +0600

    added link for AlbumItem

--- frontend/src/components/AlbumItem/AlbumItem.tsx
+++ frontend/src/components/AlbumItem/AlbumItem.tsx
@@ -1,29 +1,41 @@
-import {Grid, Typography} from '@mui/material';
-import {IAlbum} from "../../types";
-import {BASE_URL} from "../../constants.ts";
-import noImage from "../../assets/NoImage.png";
+import React, { memo } from 'react';
+import { Grid, Link, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import {BASE_URL} from '../../constants.ts';
+import noImage from '../../assets/NoImage.png';
+import {useAppSelector} from '../../app/hooks.ts';
+import {selectCurrentArtist} from '../../containers/Home/artistsSlice.ts';
+import type {IAlbum} from '../../types';
 
 
-const AlbumItem: React.FC<IAlbum> = ({ year, title, image}) => {
+const AlbumItem: React.FC<IAlbum> = memo(function AlbumItem ({_id, year, title, image}) {
+  const artist = useAppSelector(selectCurrentArtist);
+
+  const path = artist?  `/tracks/${artist._id}/${_id}` : '/';
+
   return (
-    <Grid item container>
-      <Grid item container spacing={2}>
-        <Grid item>
-          <img
-            style={{maxWidth: 200, borderRadius: 5}}
-            src={image ? BASE_URL + '/' + image : noImage}
-            alt={title}
-          />
+    <Grid item>
+      <Link to={path} component={RouterLink} sx={{textDecoration: 'none', color: 'inherit'}}>
+        <Grid item container>
+          <Grid item container spacing={2}>
+            <Grid item>
+              <img
+                style={{maxWidth: 200, borderRadius: 5}}
+                src={image ? BASE_URL + '/' + image : noImage}
+                alt={title}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant='h5'>{title}</Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography>{year}</Typography>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant='h5'>{title}</Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Typography>{year}</Typography>
-      </Grid>
+      </Link>
     </Grid>
   );
-};
+});
 
 export default AlbumItem;
