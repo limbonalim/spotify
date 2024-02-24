6d987f00cb45c26869477bd676c9c8748cfcf6da
Zhuravlev Petr <zeterfrost@gmail.com>
2024-02-24 20:12:52 +0600

    HW#86 added auth middleware and get service in trackHistory route for Api

--- api/router/trackHistory.ts
+++ api/router/trackHistory.ts
@@ -1,26 +1,14 @@
 import { Router } from 'express';
 import mongoose, { Types } from 'mongoose';
-import User from '../models/usersSchema';
 import TrackHistory from '../models/trackHistorySchema';
+import auth, { RequestWithUser } from '../middleware/auth';
 
 const trackHistoryRouter = Router();
 
-trackHistoryRouter.post('/', async (req, res, next) => {
+trackHistoryRouter.post('/', auth, async (req: RequestWithUser, res, next) => {
 	try {
-		const headerValue = req.get('Authorization');
-
-		const [_bearer, token] = headerValue ? headerValue.split(' ') : '';
-
+		const user = req.user;
 		let trackId: Types.ObjectId;
-
-		if (!token) {
-			return res.status(401).send({ message: 'No token present' });
-		}
-		const user = await User.findOne({ token });
-
-		if (!user) {
-			return res.status(401).send({ message: 'Wrong token' });
-		}
 
 		try {
 			trackId = new Types.ObjectId(req.body.track as string);
@@ -29,7 +17,7 @@ trackHistoryRouter.post('/', async (req, res, next) => {
 		}
 
 		const trackHistoryRecord = new TrackHistory({
-			user: user._id,
+			user: user?._id,
 			track: trackId,
 			datetime: new Date(),
 		});
@@ -40,6 +28,35 @@ trackHistoryRouter.post('/', async (req, res, next) => {
 		if (e instanceof mongoose.Error.ValidationError) {
 			return res.status(422).send(e);
 		}
+		next(e);
+	}
+});
+
+trackHistoryRouter.get('/', auth, async (req: RequestWithUser, res, next) => {
+	try {
+		const user = req.user;
+
+		const result = await TrackHistory.find({ user: user?._id }, null, {
+			sort: { datetime: 'desc' },
+		})
+			.populate({
+				path: 'track',
+				populate: {
+					path: 'album',
+					populate: {
+						path: 'artist',
+						select: 'name',
+					},
+				},
+			})
+			.exec();
+
+		if (!result[0]) {
+			return res.status(404).send({ message: 'Not found' });
+		}
+
+		res.send(result);
+	} catch (e) {
 		next(e);
 	}
 });
