87ba94599c81b10c2bee9ef30a35f530d7b0fafd
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-06 11:38:41 +0600

    HW#89 added theme for mui and FileInput component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './app/store.ts';
 import App from './App.tsx';
 import { addInterceptors } from './axiosApi.ts';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme.ts';
 
 addInterceptors(store);
 
@@ -14,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
+          <ThemeProvider theme={theme}>
           <App/>
+          </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
