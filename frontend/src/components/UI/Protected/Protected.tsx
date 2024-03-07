869e240e319d1110aaad44fcb161ca664ac07d8b
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-08 00:03:19 +0600

    HW#89 added admin check in Protected component for frontend

--- frontend/src/components/UI/Protected/Protected.tsx
+++ frontend/src/components/UI/Protected/Protected.tsx
@@ -2,13 +2,21 @@ import { PropsWithChildren, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../../app/hooks.ts';
 import { selectUser } from '../../../containers/Users/usersSlice.ts';
+import { Roles } from '../../../constants.ts';
 
-const Protected: React.FC<PropsWithChildren> = ({children}) => {
+interface Props extends PropsWithChildren {
+  isAdmin?: boolean
+}
+
+const Protected: React.FC<Props> = ({children, isAdmin}) => {
   const user = useAppSelector(selectUser);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) {
+      navigate('/login');
+    }
+    if (isAdmin && user?.role !== Roles.admin) {
       navigate('/login');
     }
   }, [user]);
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,6 @@
 export const BASE_URL = 'http://localhost:8000';
+
+export enum Roles {
+  user = 'user',
+  admin = 'admin'
+}
