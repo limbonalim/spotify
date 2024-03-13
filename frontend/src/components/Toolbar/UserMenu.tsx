dcd179e489005efdd3d6148942d78572949e3524
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-13 11:54:49 +0600

    HW#91 showed avatar and displayName in UserMenu component

--- frontend/src/components/Toolbar/UserMenu.tsx
+++ frontend/src/components/Toolbar/UserMenu.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Avatar, Button, Menu, MenuItem } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../app/hooks.ts';
 import { logout } from '../../containers/Users/usersThunks.ts';
 import type { IUser } from '../../types';
-import { Roles } from '../../constants.ts';
+import { BASE_URL, Roles } from '../../constants.ts';
 
 interface Props {
   user: IUser;
@@ -14,6 +15,11 @@ const UserMenu: React.FC<Props> = ({user}) => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  let avatar = user.avatar;
+  if (user.avatar && user.avatar.slice(0, 6) === 'images/') {
+    avatar = `${BASE_URL}/${user.avatar}`;
+  }
 
   const onClick = (e: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(e.currentTarget);
@@ -26,7 +32,8 @@ const UserMenu: React.FC<Props> = ({user}) => {
   return (
     <>
       <Button color="inherit" onClick={onClick}>
-        Hello, {user.username}!
+        Hello, {user.displayName}
+        {user.avatar? <Avatar alt={user.displayName} src={avatar} sx={{mx: 1}}/> : <AccountCircleIcon sx={{mx: 1}}/>}
       </Button>
       <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={onClose} keepMounted>
         <MenuItem onClick={() => dispatch(logout())}>LogOut</MenuItem>
