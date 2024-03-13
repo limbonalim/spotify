401d43f6c81e354d45a8e13c9fb3b99b31ada8d2
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-13 11:46:14 +0600

    HW#91 added dotenv for api

--- api/config.ts
+++ api/config.ts
@@ -1,11 +1,17 @@
 import path from 'path';
+import { config as envConfig } from 'dotenv';
 
+envConfig();
 const rootPath = __dirname;
 
 const config = {
 	rootPath,
 	publicPath: path.join(rootPath, 'public'),
 	mongoose: 'mongodb://localhost/spotify',
+	google: {
+		clientId: process.env['GOOGLE_CLIENT_ID'],
+		clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+	},
 };
 
 export default config;
